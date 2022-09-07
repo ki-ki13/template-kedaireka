14d6952c4099f5441c8a32574f2c074a67680ca8
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-09-07 11:51:37 +0700

    Revert "migration"
    This reverts commit e4f4148a75783e0aa93c96b34ab4b623e1d5a183.

--- coba_livewire/storage/framework/views/9eca01bff81c5113725661f7179c3865f2046a02.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
-        <span class="text-muted">&copy; 2022 Allure Alumunio, Unnes</span>
-      </div>
-    </footer>
-  </div><?php /**PATH D:\application\template\template-kedaireka\coba_livewire\resources\views/components/footer.blade.php ENDPATH**/ ?>
