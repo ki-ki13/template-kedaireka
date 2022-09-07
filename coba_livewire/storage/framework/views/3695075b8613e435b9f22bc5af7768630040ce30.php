b661ee6b059b6f59c022500702c51b51fc9ecf08
ki-ki13 <Rizkimahjati845@students.unnes.ac.id>
2022-09-07 12:08:25 +0700

    ""

---
+++ coba_livewire/storage/framework/views/3695075b8613e435b9f22bc5af7768630040ce30.php
@@ -0,0 +1,9 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<script>
+  $("#burger-menu").click(function(){
+    $(".s").toggleClass("close",[3]);
+  });
+
+</script>
+<?php /**PATH D:\Kedaireka\Coba\coba_livewire\resources\views/components/script.blade.php ENDPATH**/ ?>
---
+++ coba_livewire/storage/framework/views/773724d90ac528d26d2af31b9e05c75790edeaa8.php
@@ -0,0 +1,5 @@
+<div>
+    Coba <?php echo e($data); ?>
+
+</div>
+<?php /**PATH D:\Kedaireka\Coba\coba_livewire\resources\views/livewire/hello-world.blade.php ENDPATH**/ ?>
